3cfb0bf802260daf60fc3b47b2375574ac2be32b
Cassiano Montanari <cassianomontanari@gmail.com>
2024-07-25 00:39:04 +0200

    cleanup

--- start.sh
+++ start.sh
@@ -2,39 +2,39 @@
 
 DOTFILES_ROOT=$(pwd)
 
-echo "=============================="
-echo "Starting MACHINE configuration"
-echo "=============================="
+# echo "=============================="
+# echo "Starting MACHINE configuration"
+# echo "=============================="
 
-sh "$DOTFILES_ROOT/homebrew/install.sh"
-sh "$DOTFILES_ROOT/homebrew/apps.sh"
+# sh "$DOTFILES_ROOT/homebrew/install.sh"
+# sh "$DOTFILES_ROOT/homebrew/apps.sh"
 
-echo "====================="
-echo "Installing Oh My Zsh!"
-echo "====================="
+# echo "====================="
+# echo "Installing Oh My Zsh!"
+# echo "====================="
 
-# Remove already existing Oh My Zsh! folder
-rm -r ~/.oh-my-zsh
+# # Remove already existing Oh My Zsh! folder
+# rm -r ~/.oh-my-zsh
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-echo "============================="
-echo "Starting settings of dotfiles"
-echo "============================="
+# echo "============================="
+# echo "Starting settings of dotfiles"
+# echo "============================="
 
-echo "Backing up an existing .zshrc config"
-# Backup an existing .zshrc if exists
-if [[ -f "$HOME/.zshrc" ]]; then
-    mv -v "$HOME/.zshrc" "$HOME/.zshrc.bak"
-fi
+# echo "Backing up an existing .zshrc config"
+# # Backup an existing .zshrc if exists
+# if [[ -f "$HOME/.zshrc" ]]; then
+#     mv -v "$HOME/.zshrc" "$HOME/.zshrc.bak"
+# fi
 
-ln -s -F -i "$DOTFILES_ROOT/zsh/.zshrc" "$HOME/.zshrc"
+# ln -s -F -i "$DOTFILES_ROOT/zsh/.zshrc" "$HOME/.zshrc"
 
-echo ".zshrc file added to home"
+# echo ".zshrc file added to home"
 
-echo "==========================="
-echo "Setting configuration files"
-echo "==========================="
+# echo "==========================="
+# echo "Setting configuration files"
+# echo "==========================="
 
 mkdir -p "$HOME/.config"
 # Find all files and directories in the source directory
@@ -49,20 +49,24 @@ find "$DOTFILES_ROOT/.config" -mindepth 1 -print | while read -r file; do
   ln -s "$file" "$target"
 done
 
+# Cleanup leftovers in the original folder
+rm -rf "$DOTFILES_ROOT/.config/kitty/kitty"
+rm -rf "$DOTFILES_ROOT/.config/gh/gh"
+
 echo "Config files setup is done!"
 
-echo "============================"
-echo "Starting MacOS settings"
-echo "============================"
+# echo "============================"
+# echo "Starting MacOS settings"
+# echo "============================"
 
-# Creates a folder for Screenshots
-mkdir -p "$HOME/Screenshots"
+# # Creates a folder for Screenshots
+# mkdir -p "$HOME/Screenshots"
 
-sh macos/defaults.sh
+# sh macos/defaults.sh
 
-echo "MacOS settings is done!"
+# echo "MacOS settings is done!"
 
 
-echo "=============================="
-echo "MACHINE configuration is done!"
-echo "=============================="
+# echo "=============================="
+# echo "MACHINE configuration is done!"
+# echo "=============================="
