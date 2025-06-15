9132acb8a315f4f426d1accbaa839bb2823a15f3
Cassiano Montanari <cassianomontanari@gmail.com>
2025-06-15 16:57:41 +0200

    add apps2

--- macos/defaults.sh
+++ macos/defaults.sh
@@ -300,29 +300,10 @@ defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
 # Spotlight                                                                   #
 ###############################################################################
 
-defaults write com.apple.spotlight orderedItems -array \
-   '{"enabled" = 1;"name" = "APPLICATIONS";}' \
-   '{"enabled" = 1;"name" = "SYSTEM_PREFS";}' \
-   '{"enabled" = 1;"name" = "DIRECTORIES";}' \
-   '{"enabled" = 1;"name" = "PDF";}' \
-   '{"enabled" = 1;"name" = "FONTS";}' \
-   '{"enabled" = 0;"name" = "DOCUMENTS";}' \
-   '{"enabled" = 0;"name" = "MESSAGES";}' \
-   '{"enabled" = 0;"name" = "CONTACT";}' \
-   '{"enabled" = 0;"name" = "EVENT_TODO";}' \
-   '{"enabled" = 0;"name" = "IMAGES";}' \
-   '{"enabled" = 0;"name" = "BOOKMARKS";}' \
-   '{"enabled" = 0;"name" = "MUSIC";}' \
-   '{"enabled" = 0;"name" = "MOVIES";}' \
-   '{"enabled" = 0;"name" = "PRESENTATIONS";}' \
-   '{"enabled" = 0;"name" = "SPREADSHEETS";}' \
-   '{"enabled" = 0;"name" = "SOURCE";}' \
-   '{"enabled" = 0;"name" = "MENU_DEFINITION";}' \
-   '{"enabled" = 0;"name" = "MENU_OTHER";}' \
-   '{"enabled" = 0;"name" = "MENU_CONVERSION";}' \
-   '{"enabled" = 0;"name" = "MENU_EXPRESSION";}' \
-   '{"enabled" = 0;"name" = "MENU_WEBSEARCH";}' \
-   '{"enabled" = 0;"name" = "MENU_SPOTLIGHT_SUGGESTIONS";}'
+# Remove the Spotlight icon from the Menu
+defaults write com.apple.Spotlight MenuItemHidden -bool true
+sudo mdutil -a -i offalf
+
 # Load new settings before rebuilding the index
 killall mds
 
